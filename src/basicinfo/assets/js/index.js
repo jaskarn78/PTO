0a783da1e039fe903bfcc0b5973ad850f9376d40
Jaskarn Jagpal <jjagpal101@gmail.com>
2017-10-19 16:01:17 -0700

    Modified scripts for firebase hosting

--- src/basicinfo/assets/js/index.js
+++ src/basicinfo/assets/js/index.js
@@ -1,14 +1,4 @@
 
-var config = {
-	apiKey: "AIzaSyDXjq1Or10iD_J229t3qxyWxskmDBxjJ3s",
-	authDomain: "ptoapp-90ad1.firebaseapp.com",
-	databaseURL: "https://ptoapp-90ad1.firebaseio.com",
-	projectId: "ptoapp-90ad1",
-	storageBucket: "ptoapp-90ad1.appspot.com",
-	messagingSenderId: "101372763303"
-};
-firebase.initializeApp(config);
-
 $(document).ready(function(){
 	var data = {};
 	if(typeof(sessionStorage.getItem('userData')!=="undefined")){
--- src/welcome/assets/js/index.js
+++ src/welcome/assets/js/index.js
@@ -1,13 +1,4 @@
 
-var config = {
-	apiKey: "AIzaSyDXjq1Or10iD_J229t3qxyWxskmDBxjJ3s",
-	authDomain: "ptoapp-90ad1.firebaseapp.com",
-	databaseURL: "https://ptoapp-90ad1.firebaseio.com",
-	projectId: "ptoapp-90ad1",
-	storageBucket: "ptoapp-90ad1.appspot.com",
-	messagingSenderId: "101372763303"
-};
-firebase.initializeApp(config);
 var db = firebase.firestore();
  $(document).ready(function(){
  	if(sessionStorage.getItem('userData')!==null){
